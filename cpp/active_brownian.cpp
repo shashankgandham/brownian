ac584756264492a5d7576469236b252f4817fe98
Shashank Gandham <shashankgandham@gmail.com>
2017-09-14 10:56:54 +0530

    Replicated original file

--- cpp/active_brownian.cpp
+++ cpp/active_brownian.cpp
@@ -20,15 +20,14 @@ int main() {
 	ffv = fopen("final_fluid_velocity.dat", "w");
 	kef = fopen("ke_fluid.dat", "w");
 	kec = fopen("ke_colloid.dat", "w");
-	//zz1  = ran1(zzz);
-	if(file_colloid == 1) {
-		for(int i = 1; i <= no_of_colloid; i++)
-			fscanf(fcp, "%lf%lf%lf", &pos_colloid[3*i-2], &pos_colloid[3*i-1], &pos_colloid[3*i]);
+	for(int i = 1; i <= no_of_colloid && file_colloid; i++) {
+		fscanf(fcp, "%lf%lf%lf", &pos_colloid[3*i-2], &pos_colloid[3*i-1], &pos_colloid[3*i]);
+		fscanf(fcv, "%lf%lf%lf", &vel_colloid[3*i-2], &vel_colloid[3*i-1], &vel_colloid[3*i]);
+		fscanf(ffp, "%lf%lf%lf", &pos_fl[3*i-2], &pos_fl[3*i-1], &pos_fl[3*i]);
+		fscanf(ffv, "%lf%lf%lf", &vel_fl[3*i-2], &vel_fl[3*i-1], &vel_fl[3*i]);
 
-		for(int i = 1; i <= no_of_colloid; i++)
-			fscanf(fcv, "%lf%lf%lf", &vel_colloid[3*i-2], &vel_colloid[3*i-1], &vel_colloid[3*i]);
 	}
-	else {
+	if(!file_colloid) {
 		initialize_colloid();
 		initialize_fluid();
 	}
@@ -44,9 +43,9 @@ int main() {
 		for(int l = 1; l <= n; l++) {
 			qq = (nn  - 1)*10 + l;
 			if(nn > 90 && nn > 100000) 
-			; //write something
+			; //write something & condition check
 			std::copy(f, f + sizeof f, old_force);
-			update_pos_md;
+			update_pos_md();
 			neighbour_list_md();
 			update_pos_mpcd();
 			neighbour_list_mpcd();
@@ -57,30 +56,35 @@ int main() {
 			compute_force_md;
 			update_velocity_colloid();
 		}
-		ke_colloid = (double)0, ke_fluid = (double)0, ang_ke_colloid = (double)0;
-		mom2_x = mom2_y = mom2_z = (double)0;
+		ke_colloid = ke_fluid = ang_ke_colloid = 0;
+		mom2_x = mom2_y = mom2_z = 0;
 		for(int i = 1; i <= no_of_colloid; i++) {
 			for(int j = 0; j <= 2; j++) {
 				ke_colloid += vel_colloid[3*i - j]*vel_colloid[3*i - j];
-				ang_ke_colloid += ang_vel_colloid[3*i - j]*vel_colloid[3*i - j];
+				ang_ke_colloid += ang_vel_colloid[3*i - j]*ang_vel_colloid[3*i - j];
 			}
-			mom2_x += mass_colloid*vel_colloid[3*i - 2];
-			mom2_y += mass_colloid*vel_colloid[3*i - 1];
-			mom2_z += mass_colloid*vel_colloid[3*i];
 		}
 		ke_colloid = (double)0.5*mass_colloid*ke_colloid;
 		ang_ke_colloid = (double)0.5*I_colloid*ang_ke_colloid;
 		//write to file again :/
 		energy_colloid = potential_colloid + ke_colloid + ang_ke_colloid;
-		
-		mom1_x = mom1_y = mom1_z = (double)0;	
-		//Change this thing to strucures or something, looks horrible;	
+
 		for(int i = 1; i <= no_of_fluid; i++) {
 			for(int j = 0; j <= 2; j++) 
 				ke_fluid += vel_fl[3*i - j]*vel_fl[3*i - j];
+		}
+		ke_fluid = 0.5*ke_fluid*mass_fl;
+		mom1_x = mom1_y = mom1_z = (double)0;	
+		mom2_x = mom2_y = mom2_z = (double)0;	
+		for(int i = 1; i <= no_of_fluid; i++) {
 			mom1_x += mass_fl*vel_fl[3*i - 2];
 			mom1_y += mass_fl*vel_fl[3*i - 1];
 			mom1_y += mass_fl*vel_fl[3*i];
+		}
+		for(int i = 1; i <= no_of_colloid; i++) {
+			mom2_x += mass_colloid*vel_colloid[3*i - 2];
+			mom2_y += mass_colloid*vel_colloid[3*i - 1];
+			mom2_z += mass_colloid*vel_colloid[3*i];
 		}
 		mom_x = mom1_x + mom2_x;
 		mom_y = mom1_y + mom2_y;
