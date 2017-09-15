5723c52dd59e9576120280799dd4ddaea6f39aba
Prachi <prachipatwardhan1@gmail.com>
2017-09-15 15:54:48 +0530

    malloc changed

--- cpp/run.cpp
+++ cpp/run.cpp
@@ -1,12 +1,22 @@
 #include "parameters.hpp"
 #include <cmath>
+#include <cstdlib>
 void run() {    
-	int i,ii,j,jj,mm,cnt[no_of_colloid],nbr[7000][no_of_colloid], up_cnt[no_of_colloid],up_nbr[7000][no_of_colloid];
+	int i,ii,j,jj,mm,cnt[no_of_colloid],**nbr, up_cnt[no_of_colloid],**up_nbr;
 	double cos_b1,cos_b2,cos_b3,sin_b1,sin_b2,sin_b3, b1,b2,b3,bb1,bb2,bb3,ran1,v01,v02,v03;
 	double m11,m12,m13,m21,m22,m23,m31,m32,m33, vector_x,vector_y,vector_z,vector,dot;
 	double velx1,vely1,velz1,velx2,vely2,velz2,velx12,vely12,velz12, vv, theta,change,ke_before,ke_after,xx,vv1,vv2;
 	double up_velx2,up_vely2,up_velz2,aaa,delx,dely,delz, dumx,dumy,dumz;
 	delx=0.0;dely=0.0;delz=0.0;
+	
+	nbr = (int **)malloc(sizeof(int) * 7000);
+	for(int i = 0; i < 7000; i++) 
+		nbr[i] = (int *)malloc(sizeof(int)*(no_of_colloid));
+	
+	up_nbr = (int **)malloc(sizeof(int) * 7000);
+	for(int i = 0; i < 7000; i++) 
+		up_nbr[i] = (int *)malloc(sizeof(int)*(no_of_colloid));
+	
 	for(int i = 1; i <= no_of_colloid; i++){
 		dumx=vel_colloid[3*i-2];
 		dumy=vel_colloid[3*i-1];
@@ -50,4 +60,13 @@ void run() {
 			vel_fl[3*mm]=vel_fl[3*mm]-delz*mass_colloid/(mass_fl*float(cnt[i]));
 		}
 	}
+
+	for(int i = 0; i < 7000; i++) 
+		free(nbr[i]);
+	free(nbr);
+	
+	for(int i = 0; i < 7000; i++) 
+		free(up_nbr[i]);
+	free(up_nbr);
+		
 }
