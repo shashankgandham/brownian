4048a7196ebc3ab07e4628f82335e85cfa4cce12
Shashank Gandham <shashankgandham@gmail.com>
2017-11-11 21:47:55 +0530

    Complete indentation of Fortran

--- fortran/stochastic_reflection.f90
+++ fortran/stochastic_reflection.f90
@@ -1,59 +1,59 @@
-       subroutine stochastic_reflection
-       use all_parameters
-       implicit none
-       real*8::nx,ny,nz,unx,uny,unz,xx,m_beta,random_e,ux,uy,uz
-       real*8::uuu,utx,uty,txx,tyy,tzz,ttt,utz,ran1,val,v1,v2
-       m_beta=mass_fl/kbt
-       !find the normal unit vector from colloid surface(nx,ny,nz)!!!!!!!!!!!!!!!!!!!1
-       nx=(rsx)/dsqrt(rsx**2+rsy**2+rsz**2)
-       ny=(rsy)/dsqrt(rsx**2+rsy**2+rsz**2)
-       nz=(rsz)/dsqrt(rsx**2+rsy**2+rsz**2)
+subroutine stochastic_reflection
+	use all_parameters
+	implicit none
+	real*8::nx,ny,nz,unx,uny,unz,xx,m_beta,random_e,ux,uy,uz
+	real*8::uuu,utx,uty,txx,tyy,tzz,ttt,utz,ran1,val,v1,v2
 
-       !now generate magnitude of this unit vector from rayleigh distribution!!!!!!!!!??? Why??
-              
-        xx=ran1(zzzz)
-        random_e=(1-xx)*(1-xx)
-        val=dsqrt(-dlog(random_e)/m_beta)
+	m_beta=mass_fl/kbt
+!	!! find the normal unit vector from colloid surface(nx,ny,nz)
 
-       unx=val*nx     !un is normal velocity component after fluid colloid collision
-       uny=val*ny
-       unz=val*nz
-      
-       !now find a gaussian tangent vector to this normal!!!!!!!!!!!!!!!!!!!!!!!
-       !first find the direction of tangent
+	nx = (rsx)/dsqrt(rsx**2+rsy**2+rsz**2)
+	ny = (rsy)/dsqrt(rsx**2+rsy**2+rsz**2)
+	nz = (rsz)/dsqrt(rsx**2+rsy**2+rsz**2)
 
-       txx=ran1(zzzz)*llx 
-       tyy=ran1(zzzz)*lly       
-       tzz=ran1(zzzz)*llz
+!	!! now generate magnitude of this unit vector from rayleigh distribution !! Why??
 
-       txx=txx-rfx 
-       tyy=tyy-rfy 
-       tzz=tzz-rfz 
-                txx =txx - llx*anint(txx*inv_llx)
-                tyy =tyy - lly*anint(tyy*inv_lly)
-                tzz =tzz - llz*anint(tzz*inv_llz)
-       
-       utx=uny*tzz-tyy*unz
-       uty=unz*txx-unx*tzz
-       utz=unx*tyy-txx*uny
+	xx=ran1(zzzz)
+	random_e=(1-xx)*(1-xx)
+	val=dsqrt(-dlog(random_e)/m_beta)
 
-       uuu=dsqrt(utx*utx+uty*uty+utz*utz)
+	unx=val*nx     !un is normal velocity component after fluid colloid collision
+	uny=val*ny
+	unz=val*nz
 
-       !unit vector along tangent
-       utx=utx/uuu
-       uty=uty/uuu
-       utz=utz/uuu
-       
- 
-       call gauss(v1,v2) !magnitude of tangent
-      
+!	!! now find a gaussian tangent vector to this normal, first find the direction of tangent
 
-       utx=v1*utx !ut is tangent velocity component after fluid colloid collision
-       uty=v1*uty
-       utz=v1*utz
+	txx=ran1(zzzz)*llx
+	tyy=ran1(zzzz)*lly
+	tzz=ran1(zzzz)*llz
 
-!      write(1212,*) utx*unx+uty*uny+utz*unz
-      u1=unx+utx !x component of fluid velocity after fluid colloid collision 
-      u2=uny+uty
-      u3=unz+utz
-       end subroutine stochastic_reflection
+	txx=txx-rfx
+	tyy=tyy-rfy
+	tzz=tzz-rfz
+
+	txx =txx - llx*anint(txx*inv_llx)
+	tyy =tyy - lly*anint(tyy*inv_lly)
+	tzz =tzz - llz*anint(tzz*inv_llz)
+
+	utx = uny*tzz - tyy*unz
+	uty = unz*txx - unx*tzz
+	utz = unx*tyy - txx*uny
+	uuu = dsqrt(utx*utx + uty*uty + utz*utz)
+
+!	!!unit vector along tangent
+	utx=utx/uuu
+	uty=uty/uuu
+	utz=utz/uuu
+
+	call gauss(v1,v2) !magnitude of tangent
+
+	utx=v1*utx !ut is tangent velocity component after fluid colloid collision
+	uty=v1*uty
+	utz=v1*utz
+
+!	write(1212,*) utx*unx+uty*uny+utz*unz
+
+	u1=unx+utx !x component of fluid velocity after fluid colloid collision
+	u2=uny+uty
+	u3=unz+utz
+end subroutine stochastic_reflection
