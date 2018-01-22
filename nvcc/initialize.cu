e0af3b07c32f299279e2b72fcdcd0b1fc296ecd9
Shashank Gandham <shashankgandham@gmail.com>
2018-01-22 13:33:30 +0530

    Cuda Conversion begins, May the force be with you

--- nvcc/initialize.cu
+++ nvcc/initialize.cu
@@ -1,13 +1,13 @@
-#include "parameters.hpp"
+#include "parameters.h"
 
-void initialize() {
+__global__ void initialize() {
 	size_t devPitch;
 	cudaError_t s1, s2, s3, s4, s5, s6, s7;
 	point **ppointers[]  = {&pos_fl, &vel_fl, &dump_vel_fl, &f, &pos_colloid, &vel_colloid, &ang_vel_colloid, &old_force, &ra};
 	int   **ipointers[]  = {&fluid_no, &n_neighbour, &no_neigh, &cnt, &up_cnt};
 	int isize[]          = {(int)len.prod(), no_of_colloid};
  	int psize[]          = {no_of_fluid, no_of_colloid};
-	s1 = cudaMallocPitch(&box_part,  &devPitch, sizeof(int)*(len.prod() + 2), maxpart + 2);	
+	s1 = cudaMallocPitch(&box_part,  &devPitch, sizeof(int)*(len.prod() + 2), maxpart + 2);
 	s2 = cudaMallocPitch(&cell_part, &devPitch, sizeof(int)*(len.prod() + 2), maxpart + 2);
 	s3 = cudaMallocPitch(&box_neigh, &devPitch, sizeof(int)*(len.prod() + 2), 512);
 	s4 = cudaMallocPitch(&nbr,       &devPitch, sizeof(int)*(no_of_colloid + 2), 7002);
@@ -22,7 +22,7 @@ void initialize() {
 	}
 }
 
-void initialize_colloid() {
+__global__ void initialize_colloid() {
 	int counter = 0, check, nofp = 0, x = len.x, y = len.y, z = len.z;
 	double space_limit = 1.3*sig_colloid, ang_vscale_colloid = sqrt(12.0*kbt1/I_colloid), vscale_colloid = sqrt(12.0*kbt1/mass_colloid);
 	point avr_vel = point(0, 0, 0), t, temp, iter = point(4, 4, 4), lim = len - point(1, 1, 1);
@@ -53,7 +53,7 @@ void initialize_colloid() {
 	}
 }
 
-void initialize_fluid() {
+__global__ void initialize_fluid() {
 	int counter = 0, check;
 	double vscale_fluid = sqrt(12.0*kbt/mass_fl);
 	point avr_vel = point(0, 0, 0), t, temp;
---
+++ nvcc/parameters.h
@@ -0,0 +1,77 @@
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <climits>
+#include <curand.h>
+
+__device__ extern int n, niter, file, nbin, no_of_fluid, maxpart, no_of_colloid, nbox, **nbr, **up_nbr, *cnt, *up_cnt, *iv, ntab;
+__device__ extern int **neighbour, *n_neighbour, *no_neigh, **neigh_fl, **box_neigh, **box_part, *fluid_no, **cell_part, seed, nn;
+__device__ extern double kbt, kbt1, ndt, dt, mass_colloid, sig_colloid, eps, v0, sigma, dv, mass_fl, I_colloid, potential_colloid;
+
+__global__ void create_box(), compute_force_md(), fluid_colloid_collision(), initialize(), initialize_fluid(), initialize_colloid();
+__global__ void neighbour_list_md(), neighbour_list_mpcd(), rotation_mpcd(), run(), tumble(), updown_velocity();
+__global__ void update_velocity_colloid(), update_pos_md(), update_pos_mpcd() ,update_activity_direction();
+
+__global__ inline double ran() {
+	__device__ static int im1 = 2147483563, im2 = 2147483399, ia1 = 40014, ia2 = 40692, iq1 = 53668, iq2 = 52774, iy, j, k;
+	__device__ static int imm = im1 - 1, ir1 = 12211, ir2 = 3791, ntab = 32, ndiv = 1 + imm/ntab, idum = 123456789;
+	__device__ double eps = 1.2e-7, rnmx = 1 - eps, am = 1.0/im1;
+	if(seed <= 0) {
+		seed = std::max(-seed,1);
+		idum = seed;
+		for(j = ntab + 8; j >= 1; j--) {
+			k = seed/iq1;
+			seed = ia1*(seed - k*iq1) - k*ir1;
+			if(seed < 0) seed += im1;
+			if(j <= ntab) iv[j] = seed;
+		}
+		iy = iv[1];
+	}
+	k = seed/iq1;
+	seed = ia1*(seed - k*iq1) - k*ir1;
+	if(seed < 0) seed += im1;
+	k = idum/iq2;
+	idum = ia2*(idum - k*iq2) - k*ir2;
+	if(idum < 0) idum = idum + im2;
+	j = 1 + iy/ndiv;
+	iy = iv[j] - idum;
+	iv[j] = seed;
+	if(iy < 1) iy = iy + imm;
+	return std::min(am*iy, rnmx);
+}
+
+__device__ struct point {
+	double x, y, z;
+	CUDA_CALLABLE_MEMBER inline point(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z){}
+	CUDA_CALLABLE_MEMBER inline point operator+ (const point &op) { return point(x + op.x, y + op.y, z + op.z); }
+	CUDA_CALLABLE_MEMBER inline point operator- (const point &op) { return point(x - op.x, y - op.y, z - op.z); }
+	CUDA_CALLABLE_MEMBER inline point operator/ (const double &op){ return point(x / op,   y / op,   z / op);   }
+	CUDA_CALLABLE_MEMBER inline point operator/ (const point &op) { return point(x / op.x, y / op.y, z / op.z); }
+	CUDA_CALLABLE_MEMBER inline point operator* (const point &op) { return point(x * op.x, y * op.y, z * op.z); }
+	CUDA_CALLABLE_MEMBER inline point operator* (const double &op){ return point(x * op, y * op, z * op); }
+	CUDA_CALLABLE_MEMBER inline point operator+=(const point &op) { x += op.x, y += op.y, z += op.z; return *this; }
+	CUDA_CALLABLE_MEMBER inline point operator-=(const point &op) { x -= op.x, y -= op.y, z -= op.z; return *this; }
+	CUDA_CALLABLE_MEMBER inline bool operator==(const point &op) { return (x == op.x && y == op.y && z == op.z); }
+
+	CUDA_CALLABLE_MEMBER inline double sum()  { return (x + y + z); }
+    CUDA_CALLABLE_MEMBER inline double prod() { return (x * y * z); }
+	CUDA_CALLABLE_MEMBER inline int cell(point len) { return int(x) + len.x*int(y) + len.x*len.y*int(z); }
+	CUDA_CALLABLE_MEMBER inline void print(FILE *fp = stdout)  { fprintf(fp, "%36.32lf %35.32lf %35.32lf\n", x, y, z); }
+	CUDA_CALLABLE_MEMBER inline point random(point dec = point(0, 0 ,0), point mul = point(1, 1, 1)) {
+		x = ran(); y = ran(); z = ran();
+		*this = (*this)*mul - dec;
+		return *this;
+	}
+	CUDA_CALLABLE_MEMBER inline void next(point len, point inc = point(1, 1, 1), point start = point(1, 1, 1)) {
+		x += inc.x;
+		if(x > len.x) y += inc.y, x = start.x;
+		if(y > len.y) z += inc.z, y = start.y;
+	}
+};
+__global__ extern point *pos_colloid, *pos_fl, *vel_colloid, *vel_fl, *ang_vel_colloid, *f, *old_force, *ra, len, *dump_vel_fl, *cell_vel;
+
+inline point round(point a) { return ((point(lround(a.x), lround(a.y), lround(a.z)))); }
+inline point mod(point a, point b)  { return a - b*(round((a - b/2)/b)); }
+inline point img(point a, point b)  { return a - b*round(a/b); }
