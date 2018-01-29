01fa3d995fafade68a59e703e8011d261a844eb2
shashankgandham <shashankgandham@gmail.com>
2018-01-29 22:15:49 +0530

    Revert "snvcc without errors"
    This reverts commit bcd329e39ddc4b97e08935baca124232c6ab06d4.

--- snvcc/fluid_colloid_collision.cpp
+++ snvcc/fluid_colloid_collision.cpp
@@ -24,9 +24,9 @@ inline point stochastic_reflection(point rf, point rs) {
 }
 
 void fluid_colloid_collision() {
-	point rr, rs, *dump_vel_fl, u, omega, vc;
-	dump_vel_fl = (point *)malloc((no_of_fluid + 2)*sizeof(point));
+	point rr, rs, dump_vel_fl[no_of_fluid + 1], u, omega, vc;
 	std::copy(vel_fl, vel_fl + no_of_fluid + 1, dump_vel_fl);
+
 	for (int j = 1; j <= no_of_colloid; j++) {
 		vc = omega = point(0, 0, 0);
 		for (int i = 1; i <= no_neigh[j]; i++) {
