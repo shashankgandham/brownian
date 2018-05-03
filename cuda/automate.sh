6921ec84ce871c76681e28c4c0f29e410a3b81ba
Shashank Gandham <shashankgandham@gmail.com>
2018-05-03 14:36:28 +0000

    Added automate for cuda

--- cuda/automate.sh
+++ cuda/automate.sh
@@ -1,9 +1,12 @@
+sleep 300
 for density in 5 10 20 
 do
 	for colloid in {1..10} 
 	do
 		echo "nvprof --log-file vprof$density$colloid.txt ./a.out $density $colloid"
-		nvprof --log-file vprof$density$colloid.txt ./a.out $density $colloid 
+		sudo nice -n -20 ./a.out $density $colloid > prof$density$colloid.txt
+		sudo nice -n -20 /usr/local/cuda-9.1/bin/nvprof --log-file vprof$density$colloid.txt ./a.out $density $colloid 
+		cat vprof$density$colloid.txt >> prof$density$colloid.txt
 		rm -rf /tmp/.nvprof/*
 	done
 done
