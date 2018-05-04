e5f70893b5346298b2d883e131a841db64cac134
Shashank Gandham <shashankgandham@gmail.com>
2018-05-04 06:01:23 +0000

    Final Reports Added

--- cpp/automate.sh
+++ cpp/automate.sh
@@ -1,10 +1,11 @@
-for density in 5
+sleep 300
+for density in 10
 do
 	for colloid in {1..10} 
 	do
 		echo "Running $density $colloid"
-		sudo nice -n -19 ./a.out $density $colloid > prof$density$colloid.txt
-		gprof gmon.out > gmon$density$colloid.txt
+		sudo nice -n -20 ./a.out $density $colloid > prof$density$colloid.txt
+		gprof a.out > gmon$density$colloid.txt
 		cat gmon$density$colloid.txt >> prof$density$colloid.txt
 	done
 done
